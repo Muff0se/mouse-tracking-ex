8b536b7006c3f3f6fcf57f0337e22a972149ea90
Granata <yournarrator013@gmail.com>
2025-04-13 15:19:41 -0300

    partially-working

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,9 @@
 const mousePosTrack = document.getElementById('mousePosTrack');
+const cathMeButton = document.getElementById("cathMeButton");
+
+cathMeButton.addEventListener("mouseenter", () => {
+    randomButtonPosition(0, 1075, 0, 703);
+})
 
 document.addEventListener("mousemove", () => {
     const x = event.clientX;
@@ -6,3 +11,12 @@ document.addEventListener("mousemove", () => {
 
     mousePosTrack.textContent = `X: ${x} / Y: ${y}`;
 })
+
+function randomButtonPosition (minX, maxX, minY, maxY) {
+    let left = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
+    let top = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+
+    cathMeButton.style.left = left + 'px';
+    cathMeButton.style.top = top + 'px';
+}
+
